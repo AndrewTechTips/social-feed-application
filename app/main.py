5c70e513241cabbd80e4dc983d71b4fb560dcbfc
AndrewTechTips <andreww.steel69@gmail.com>
2026-07-17 18:37:49 +0300

    establish database connection with error handling in FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from http.client import HTTPException
 from typing import Optional
+import time
 
 from fastapi import FastAPI, Response, status, HTTPException
-from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
@@ -14,6 +16,24 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
+
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="fastapi",
+            user="postgres",
+            password="123454321",
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("Database connection was succesfull!")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print("Error: ", error)
+        time.sleep(2)
 
 
 my_posts = [
