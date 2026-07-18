4f094ae9d2486d046c6f98d0eb5e449cda9a7d9a
AndrewTechTips <andreww.steel69@gmail.com>
2026-07-18 16:31:15 +0300

    refactor password hashing into a utility function

--- app/main.py
+++ app/main.py
@@ -1,16 +1,14 @@
 import psycopg2
 import time
 
-from passlib.context import CryptContext
 from http.client import HTTPException
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from psycopg2.extras import RealDictCursor
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 from typing import List
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -129,7 +127,7 @@ def update_post(
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
-    hashed_password = pwd_context.hash(user.password)
+    hashed_password = utils.hash(user.password)
     user.password = hashed_password
 
     new_user = models.User(**user.model_dump())
