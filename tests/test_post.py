47e03b370405bb37fedc574ff741d3910937c129
AndrewTechTips <andreww.steel69@gmail.com>
2026-07-22 19:48:58 +0300

    feat: add additional user fixture and enhance post creation tests

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,3 +1,5 @@
+import pytest
+
 from app import schemas
 
 
@@ -33,3 +35,66 @@ def test_get_one_post(authorized_client, test_posts):
     assert post.Post.id == test_posts[0].id
     assert post.Post.content == test_posts[0].content
     assert post.Post.title == test_posts[0].title
+
+
+@pytest.mark.parametrize(
+    "title, content, published",
+    argvalues=[
+        ("awesome new title", "awesome new content", True),
+        ("favorite pizza", "i love pepperoni", False),
+        ("tallest skyscrapers", "wahoooya", True),
+    ],
+)
+def test_create_post(
+    authorized_client, test_user, test_posts, title, content, published
+):
+    res = authorized_client.post(
+        "/posts/", json={"title": title, "content": content, "published": published}
+    )
+
+    created_post = schemas.Post(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == title
+    assert created_post.content == content
+    assert created_post.published == published
+    assert created_post.user_id == test_user["id"]
+
+
+def test_create_post_default_published_true(authorized_client, test_user):
+    res = authorized_client.post(
+        "/posts/", json={"title": "arbitrary title", "content": "Karaganda"}
+    )
+
+    created_post = schemas.Post(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == "arbitrary title"
+    assert created_post.content == "Karaganda"
+    assert created_post.published == True
+    assert created_post.user_id == test_user["id"]
+
+
+def test_unauthorized_user_create_posts(client, test_user):
+    res = client.post(
+        "/posts/", json={"title": "arbitrary title", "content": "Karaganda"}
+    )
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_delete_post(client, test_user, test_posts):
+    res = client.delete(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_delete_post_success(authorized_client, test_user, test_posts):
+    res = authorized_client.delete(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 204
+
+
+def test_delete_post_non_exist(authorized_client, test_user, test_posts):
+    res = authorized_client.delete("/posts/312313")
+    assert res.status_code == 404
+
+
+def test_delete_other_user_post(authorized_client, test_user, test_posts):
+    res = authorized_client.delete(f"/posts/{test_posts[3].id}")
+    assert res.status_code == 403
