943682e673da53f015bdda122c4da48833363565
AndrewTechTips <andreww.steel69@gmail.com>
2026-08-09 17:23:25 +0300

    feat: refactor post endpoints to use SQLAlchemy select statements for improved performance

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,11 +1,11 @@
-from .. import models, schemas, oauth2
-from fastapi import status, HTTPException, Response, Depends, APIRouter
-from sqlalchemy.orm import Session
-from ..database import get_db
 from typing import List, Optional
-from sqlalchemy import func
 
-from ..oauth2 import get_current_user
+from fastapi import status, HTTPException, Response, Depends, APIRouter
+from sqlalchemy import select, func
+from sqlalchemy.orm import Session
+
+from .. import models, schemas, oauth2
+from ..database import get_db
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
 
@@ -18,19 +18,17 @@ def get_posts(
     skip: int = 0,
     search: Optional[str] = "",
 ):
-    # cursor.execute("""SELECT * FROM posts""")
-    # posts = cursor.fetchall()
 
-    posts = (
-        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+    stmt = (
+        select(models.Post, func.count(models.Vote.post_id).label("votes"))
         .join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True)
         .group_by(models.Post.id)
-        .filter(models.Post.title.contains(search))
+        .where(models.Post.title.contains(search))
         .limit(limit)
         .offset(skip)
-        .all()
     )
 
+    posts = db.execute(stmt).all()
     return posts
 
 
@@ -40,17 +38,11 @@ def create_posts(
     db: Session = Depends(get_db),
     current_user: models.User = Depends(oauth2.get_current_user),
 ):
-    # cursor.execute(
-    #     """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-    #     (post.title, post.content, post.published),
-    # )
-    # new_post = cursor.fetchone()
-    # conn.commit()
+
     new_post = models.Post(user_id=current_user.id, **post.model_dump())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-
     return new_post
 
 
@@ -60,23 +52,21 @@ def get_post(
     db: Session = Depends(get_db),
     current_user: models.User = Depends(oauth2.get_current_user),
 ):
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id),))
-    # post = cursor.fetchone()
 
-    post = (
-        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+    stmt = (
+        select(models.Post, func.count(models.Vote.post_id).label("votes"))
         .join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True)
         .group_by(models.Post.id)
-        .filter(models.Post.id == id)
-        .first()
+        .where(models.Post.id == id)
     )
+
+    post = db.execute(stmt).first()
 
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id: {id} was not found",
         )
-
     return post
 
 
@@ -86,12 +76,8 @@ def delete_post(
     db: Session = Depends(get_db),
     current_user: models.User = Depends(oauth2.get_current_user),
 ):
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
+    stmt = select(models.Post).where(models.Post.id == id)
+    post = db.scalar(stmt)
 
     if post is None:
         raise HTTPException(
@@ -105,9 +91,8 @@ def delete_post(
             detail="Not authorized to perform requested action",
         )
 
-    post_query.delete(synchronize_session=False)
+    db.delete(post)
     db.commit()
-
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
@@ -118,16 +103,8 @@ def update_post(
     db: Session = Depends(get_db),
     current_user: models.User = Depends(oauth2.get_current_user),
 ):
-
-    # cursor.execute(
-    #     """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-    #     (post.title, post.content, post.published, str(id)),
-    # )
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
+    stmt = select(models.Post).where(models.Post.id == id)
+    post = db.scalar(stmt)
 
     if post is None:
         raise HTTPException(
@@ -141,7 +118,10 @@ def update_post(
             detail="Not authorized to perform requested action",
         )
 
-    post_query.update(updated_post.model_dump(), synchronize_session=False)
-    db.commit()
+    for key, value in updated_post.model_dump().items():
+        setattr(post, key, value)
 
-    return post_query.first()
+
+    db.commit()
+    db.refresh(post)
+    return post
