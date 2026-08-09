1b7ccc2cf303b197e51f663afc8b872a02b497f5
AndrewTechTips <andreww.steel69@gmail.com>
2026-08-09 17:33:14 +0300

    feat: refactor vote handling to use SQLAlchemy select statements for improved performance

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,5 +1,7 @@
-from fastapi import status, HTTPException, Response, Depends, APIRouter
+from fastapi import status, HTTPException, Depends, APIRouter
+from sqlalchemy import select
 from sqlalchemy.orm import Session
+
 from .. import schemas, database, models, oauth2
 
 router = APIRouter(prefix="/vote", tags=["Vote"])
@@ -12,17 +14,19 @@ def vote(
     current_user: models.User = Depends(oauth2.get_current_user),
 ):
 
-    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    stmt_post = select(models.Post).where(models.Post.id == vote.post_id)
+    post = db.scalar(stmt_post)
+
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id: {vote.post_id} does not exist",
         )
 
-    vote_query = db.query(models.Vote).filter(
+    stmt_vote = select(models.Vote).where(
         models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id
     )
-    found_vote = vote_query.first()
+    found_vote = db.scalar(stmt_vote)
 
     if vote.dir == 1:
         if found_vote:
@@ -33,14 +37,12 @@ def vote(
         new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
         db.add(new_vote)
         db.commit()
-
         return {"message": "Successfully added vote"}
     else:
         if not found_vote:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND, detail="Vote does not exist"
             )
-        vote_query.delete(synchronize_session=False)
+        db.delete(found_vote)
         db.commit()
-
         return {"message": "successfully deleted vote"}
